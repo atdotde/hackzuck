10bdebc36674355f63e37f86fb8ee50c3219392e
Robert C. Helling <helling@atdotde.de>
2023-12-26 15:25:22 +0100

    Punkte zurueck und Begriffsauswahl
    Signed-off-by: Robert C. Helling <helling@atdotde.de>

--- hackzuck.pl
+++ hackzuck.pl
@@ -5,6 +5,8 @@ use Time::HiRes;
 use strict;
 
 initscr;
+start_color;
+
 curs_set(0); # hide cursor
 my $win = Curses->new;
 
@@ -15,6 +17,13 @@ keypad(stdscr, 1);
 
 
 sub DESTROY { echo(); curs_set(1); endwin; }
+
+unless(has_colors()) {
+  die "No colors!";
+}
+
+init_pair(1, COLOR_RED, COLOR_BLUE);
+attron(COLOR_PAIR(2));
 
 my $timer = newwin(3,10,1,1);
 
@@ -28,7 +37,11 @@ my $punkteB = 0;
 &updateB;
 
 my $center = newwin(5, 40, LINES() / 2, COLS() / 2 - 20);
-#$center->attron(A_REVERSE);
+$center->attron(A_BOLD);
+
+my $upper = newwin(5, 40, LINES() / 2 - 5, COLS() / 2 - 20);
+$upper->attron(A_BOLD);
+
 
 my $begriff = "JEOPARDY";
 
@@ -40,32 +53,56 @@ refresh;
 
 $SIG{ALRM} = sub {--$t; $timer->addstring(0,0,"Time: $t  "); $timer->refresh; Time::HiRes::alarm(1);};
 
-Time::HiRes::alarm(1);
+
 
 $center->border('|', '|', '-', '-', '+', '+', '+', '+');
 &alert($begriff);
+
+$upper->border('|', '|', '-', '-', '+', '+', '+', '+');
+&ualert("HACKER");
 
 while (1) {
   my $c = getch();
   if ($c eq 'a') {
     ++$punkteA;
     &updateA;
-  } elsif ($c eq 'b') {
+  } elsif ($c eq 'A') {
+    --$punkteA;
+    &updateA;
+  }
+  elsif ($c eq 'b') {
     ++$punkteB;
     &updateB;
+  } elsif ($c eq 'B') {
+    --$punkteB;
+    &updateB;
+  } elsif ($c eq '1') {
+    $begriff = "HACKER";
+    $upper->clear;
+    $upper->refresh;
+    &alert($begriff);
+    Time::HiRes::alarm(1);
+  } elsif ($c eq '2') {
+    $upper->clear;
+    $upper->refresh;
+    &alert($begriff);
+    Time::HiRes::alarm(1);    
   } elsif ($c eq ' ') {
     Time::HiRes::alarm(0);
     alert("HORN");
   } elsif ($c eq 'q') {
     exit;
   } elsif ($c eq 'r') {
+    $begriff = "JEOPARDY";
+    $upper->border('|', '|', '-', '-', '+', '+', '+', '+');
+    &ualert("HACKER");
     $center->clear;
     $center->border('|', '|', '-', '-', '+', '+', '+', '+');
     &alert($begriff);
     $center->refresh;
     $t = 15;
     $timer->addstring(0,0,"Time: $t    ");
-    Time::HiRes::alarm(1);
+    Time::HiRes::alarm(0);
 
     $punkteA = 0;
     $punkteB = 0;
@@ -97,3 +134,11 @@ sub alert {
   $center->addstring(2,2, $pad . $msg . $pad);
   $center->refresh;
 }
+
+sub ualert {
+  my $msg = shift;
+  my $pad = ' ' x ((37 - length($msg))/2);
+  $upper->addstring(2,2, $pad . $msg . $pad);
+  $upper->refresh;
+}
+  
